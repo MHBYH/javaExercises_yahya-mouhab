94cb97368257889ee77f42ff5d9a80b9b3e8aef7
YAHYA HFAIED <hfaiedyahya.mouhab@gmail.com>
2023-06-22 14:44:20 +0200

    J'ai créé 3 fichier  pour la methode get de la route user

---
+++ src/test/java/fr/yahya/myApi/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yahya.myApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
